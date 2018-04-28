1da020cfeb643ec16c9674691f9121565e9f5e44
elcimax <elcimax@tiscali.it>
2018-04-28 14:16:57 +0200

    first commit

--- api/routes/AmbientRoutes.js
+++ api/routes/AmbientRoutes.js
@@ -1,13 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var dateFormat = require('dateformat');
+const Influx = require('influx')
+const influx = new Influx.InfluxDB({
+  host: 'influxdb-svc.ambientdata.svc',
+  database: 'cam42DB',
+  schema: [
+    {
+      measurement: 'ambient',
+      fields: {
+        temp: Influx.FieldType.INTEGER,
+        hum: Influx.FieldType.INTEGER
+      },
+      tags: [
+        'host'
+      ]
+    }
+  ]
+})
 
-router.get("/insert", (req, response) => {
-    var resp={};
-    resp.result="OK";
-    resp.timestamp=dateFormat(new Date(), "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'")
-    response.status( 200 ).json(resp);
+    router.post("/insert", (req, response) => {
+        let temp = req.body.temp;
+        let hum = req.body.hum;
+        console.log(`Write to ${temp} ${hum}`);
 
-});
+        influx.writePoints([{
+            measurement: 'ambient',
+            fields: { temp:temp, hum:hum }
+            }
+        ]).catch(err => {
+          console.error(`Error saving data to InfluxDB! ${err.stack}`)
+        })
+
+
+        var resp={};
+        resp.result="OK";
+        resp.timestamp=dateFormat(new Date(), "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'")
+        response.status( 200 ).json(resp);
+
+    });
+
+
+    router.post("/select", (req, response) => {
+        let str="SELECT MEAN(temperature) as temperature,MEAN(humidity) as humidity " +
+                            "FROM ambient " +
+                            //"WHERE time > "+from+" and time <= "+ to + " "+
+                            "GROUP BY time(60m)";
+
+    influx.query(str).then(result => {
+        response.json(result)
+      }).catch(err => {
+        response.status(500).send(err.stack)
+      })
+    });
+
 
 module.exports = router;
