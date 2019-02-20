1edb0a2252a52b82db41cb5484c529c20e609c9d
Alessio Cimadon <alessio.cimadon@assembly.it>
2019-02-20 16:42:08 +0100

    Porting a postgres

--- api/routes/AmbientRoutes.js
+++ api/routes/AmbientRoutes.js
@@ -54,8 +54,9 @@ ssl: true
         if(to.indexOf("now")<0){
           to="'"+to+"'";
         }
-        let str="select temp/measure_count,hum/measure_count from ambient_data";
-	pool.query(now_query, (err, res) => {
+        let str="select temp/measure_count as temp,hum/measure_count as hum from ambient_data";
+	pool.query(str, (err, res) => {
+		console.log(res);
 		if(res!=null && res.rowCount>0){
 			response.json(res.rows);
 		}else{
